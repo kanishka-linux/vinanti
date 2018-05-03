db4ffa539a9ebf7b4b080307e06ba8ae99de4518
kanishka-linux <kanishka.linux@gmail.com>
2018-05-03 17:24:16 +0530

    add logger

---
+++ vinanti/log.py
@@ -0,0 +1,15 @@
+import logging
+
+def log_function(name):
+    logging.basicConfig(level=logging.DEBUG)
+    logging.getLogger('asyncio').disabled = True
+    #fmt = '%(asctime)-15s::%(module)s:%(funcName)s: %(levelname)-7s - %(message)s'
+    #formatter_ch = logging.Formatter(fmt)
+    fmt = '%(levelname)s::%(module)s::%(funcName)s: %(message)s'
+    formatter_ch = logging.Formatter(fmt)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    ch.setFormatter(formatter_ch)
+    logger = logging.getLogger(name)
+    logger.addHandler(ch)
+    return logger
