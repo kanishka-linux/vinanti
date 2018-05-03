69a04d3ee0a9f536cfe354332075513bb1828e4b
kanishka-linux <kanishka.linux@gmail.com>
2018-05-03 17:52:34 +0530

    set asyncio log level to warning

--- vinanti/log.py
+++ vinanti/log.py
@@ -2,7 +2,7 @@ import logging
 
 def log_function(name):
     logging.basicConfig(level=logging.DEBUG)
-    logging.getLogger('asyncio').disabled = True
+    logging.getLogger('asyncio').setLevel(logging.WARNING)
     #fmt = '%(asctime)-15s::%(module)s:%(funcName)s: %(levelname)-7s - %(message)s'
     #formatter_ch = logging.Formatter(fmt)
     fmt = '%(levelname)s::%(module)s::%(funcName)s: %(message)s'
