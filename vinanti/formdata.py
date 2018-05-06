c1a64ed02ef566406dd5698f4ad175af42977f4d
kanishka-linux <kanishka.linux@gmail.com>
2018-05-06 17:50:19 +0530

    change boundary value

--- vinanti/formdata.py
+++ vinanti/formdata.py
@@ -19,7 +19,6 @@ along with vinanti.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
 import uuid
-import hashlib
 import mimetypes
 
 class Formdata:
@@ -29,9 +28,8 @@ class Formdata:
         self.file_dict = file_dict
         self.final_list = []
         boundary = str(uuid.uuid4())
-        boundary_bytes = bytes(boundary, 'utf-8')
-        h = hashlib.sha256(boundary_bytes)
-        self.boundary = h.hexdigest()
+        boundary = boundary.replace('-', '')
+        self.boundary = '----------' + boundary
     
     def get_content_type(self, filename):
         return mimetypes.guess_type (filename)[0] or 'application/octet-stream'
@@ -75,9 +73,9 @@ class Formdata:
         self.final_list.append(bytes(boundary+'--', 'utf-8'))
         self.final_list.append(b'')
         body = b'\r\n'.join (self.final_list)
-        headers = {
+        hdrs = {
             'Content-Type': 'multipart/form-data; boundary={}'.format(self.boundary),
             'Content-Length': str(len(body))
             }
-        return body, headers
+        return body, hdrs
         
