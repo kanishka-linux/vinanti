e3871f29b64a7cbb1a84776f1c0169bff0d1f1b8
kanishka-linux <kanishka.linux@gmail.com>
2018-05-11 03:16:52 +0530

    modify test_wait

--- tests/test_wait.py
+++ tests/test_wait.py
@@ -15,30 +15,33 @@ def namaste(*args):
 def konichiwa(*args):
     print('konichiwa: {}'.format(args[-2]))
 
-logval = True
 
 class TestVinanti(unittest.TestCase):
     
-    def test_add_block(self):
-        vnt = Vinanti(block=True, log=logval, hdrs=hdr)
+    def test_add_noblock(self):
+        vnt = Vinanti(block=False, log=False, hdrs=hdr, wait=1.0)
         vnt.get('http://www.google.com',onfinished=hello)
-        vnt.get('http://www.wikipedia.org',onfinished=namaste)
-        vnt.get('http://www.duckduckgo.com',onfinished=konichiwa)
-        vnt.get('http://www.google.com',onfinished=hello, wait=3.0)
-        vnt.get('http://www.wikipedia.org',onfinished=namaste)
-        vnt.get('http://www.duckduckgo.com',onfinished=konichiwa)
-        #vnt.start()
-        
-    def test_add_block(self):
-        vnt = Vinanti(block=False, log=logval, hdrs=hdr)
         vnt.get('http://www.google.com',onfinished=hello)
+        vnt.get('http://www.google.com',onfinished=hello)
+        vnt.get('http://www.google.com',onfinished=hello)
+        vnt.get('http://www.google.com',onfinished=hello)
+        vnt.get('http://www.google.com',onfinished=hello)
+        vnt.get('http://www.google.com',onfinished=hello)
+        vnt.get('http://www.duckduckgo.com',onfinished=konichiwa)
+        vnt.get('http://www.duckduckgo.com',onfinished=konichiwa)
+        vnt.get('http://www.duckduckgo.com',onfinished=konichiwa)
+        vnt.get('http://www.duckduckgo.com',onfinished=konichiwa)
+        vnt.get('http://www.duckduckgo.com',onfinished=konichiwa)
+        vnt.get('http://www.duckduckgo.com',onfinished=konichiwa)
+        vnt.get('http://www.duckduckgo.com',onfinished=konichiwa)
         vnt.get('http://www.wikipedia.org',onfinished=namaste)
-        vnt.get('http://www.duckduckgo.com',onfinished=konichiwa)
-        vnt.get('http://www.google.com',onfinished=hello, wait=3.0)
-        vnt.get('http://www.wikipedia.org',onfinished=namaste, wait=1.0)
-        vnt.get('http://www.duckduckgo.com',onfinished=konichiwa)
+        vnt.get('http://www.wikipedia.org',onfinished=namaste)
+        vnt.get('http://www.wikipedia.org',onfinished=namaste)
+        vnt.get('http://www.wikipedia.org',onfinished=namaste)
+        vnt.get('http://www.wikipedia.org',onfinished=namaste)
+        vnt.get('http://www.wikipedia.org',onfinished=namaste)
+        vnt.get('http://www.wikipedia.org',onfinished=namaste)
     
-        
 if __name__ == '__main__':
     BASEDIR, BASEFILE = os.path.split(os.path.abspath(__file__))
     parent_basedir, __ = os.path.split(BASEDIR)
