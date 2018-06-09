25f1a0f59479875de3d92c1f102910d7b0e6ad34
kanishka-linux <kanishka.linux@gmail.com>
2018-06-09 14:00:57 +0530

    add test for urllib

--- tests/test_crawl.py
+++ tests/test_crawl.py
@@ -8,17 +8,29 @@ import time
 hdr = {"User-Agent":"Mozilla/5.0"}
 
 def hello(*args):
-    print('hello: {} {}'.format(args[0], args[1]))
+    vnt = args[0]
+    print('hello: {} {}'.format(args[1], args[2]))
+    print(vnt.tasks_count(), vnt.tasks_done(), vnt.tasks_remaining())
 
+def hello_world(*args):
+    vnt = args[0]
+    print('hello_world: {} {}'.format(args[0], args[1]))
 
 class TestVinanti(unittest.TestCase):
     
     def test_crawl(self):
-        vnt = Vinanti(block=False, backend='aiohttp', old_method=False, max_requests=5,
-                      hdrs=hdr, session=True, onfinished=hello, loop_forever=False, wait=0.2)
+        vnt = Vinanti(block=False, backend='aiohttp', max_requests=10,
+                      hdrs=hdr, session=True, loop_forever=False, wait=0.2)
+        url = 'https://docs.python.org/3/reference/index.html'
+        vnt.crawl(url, onfinished=partial(hello, vnt))
+    
+    def test_crawl_urllib(self):
+        vnt = Vinanti(block=False, backend='urllib', max_requests=5,
+                      hdrs=hdr, session=True, onfinished=hello_world,
+                      loop_forever=False, wait=0.2)
         url = 'https://docs.python.org/3/reference/index.html'
         vnt.crawl(url)
-        
+    
 if __name__ == '__main__':
     BASEDIR, BASEFILE = os.path.split(os.path.abspath(__file__))
     parent_basedir, __ = os.path.split(BASEDIR)
